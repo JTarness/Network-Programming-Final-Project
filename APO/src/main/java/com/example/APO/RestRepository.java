979adefb6b06f8be07838f11bf85de3943b82b70
JTarness <30990604+JTarness@users.noreply.github.com>
2019-12-12 18:22:45 -0500

    Add files via upload

---
+++ APO/src/main/java/com/example/APO/RestRepository.java
@@ -0,0 +1,9 @@
+package com.example.APO;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface RestRepository extends CrudRepository<User, Long>{
+
+}
---
+++ APO/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+
+
+INSERT INTO User(name, email, number) VALUES('tarnessj','tarnessj1@gmail.com',603-327-5451);
+ 
+ COMMIT;
